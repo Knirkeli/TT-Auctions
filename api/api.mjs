06ee11415ba1c81651418fb6a89b0670a3dea444
Eirik Kirkeli <knirkeli88@msn.com>
2023-12-05 17:52:30 +0100

    adde api base

---
+++ api/api.mjs
@@ -0,0 +1,6 @@
+// Declare a constant `apiBase` and assign it the base URL of the API.
+const apiBase = 'https://api.noroff.dev/api/v1';
+
+// Declare a constant `auctionEndpoint` and assign it the full URL of the auction endpoint.
+// `export` makes `auctionEndpoint` available to be imported by other modules.
+export const auctionEndpoint = `${apiBase}/auction`;
