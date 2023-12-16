8f900328dd3d280632ce57db8a1654572cd532f8
Eirik Kirkeli <knirkeli88@msn.com>
2023-12-16 19:27:59 +0100

    Refactor header links and update gold count display

--- Lander/fetchposts.mjs
+++ Lander/fetchposts.mjs
@@ -1,39 +1,3 @@
-// import { auctionEndpoint } from '../api/api.mjs';
-
-// export async function fetchListings() {
-//     const response = await fetch(`${auctionEndpoint}/listings`);
-//     const listings = await response.json();
-//     return listings;
-// }
-
-//   async function displayLatestListings() {
-//     const listings = await fetchListings();
-//     const sortedListings = listings.sort((a, b) => new Date(b.created) - new Date(a.created));
-//     const latestListings = sortedListings.slice(0, 8);
-    
-//     // TODO: Display the latest listings in the HTML
-//   }
-
-
-// function startTimer(endTime) {
-//     const timerElement = document.getElementById('timer');
-//     const end = new Date(endTime);
-  
-//     setInterval(() => {
-//       const now = new Date();
-//       const distance = end - now;
-  
-//       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-//       timerElement.textContent = `${hours}:${minutes}:${seconds}`;
-//     }, 1000);
-//   }
-  
-//   // Start the timer for the first listing
-//   startTimer(latestListings[0].endsAt);
-
 import { auctionEndpoint } from '../api/api.mjs';
 
 export async function fetchListings(limit = 100, offset = 0) {
--- profile/gold.mjs
+++ profile/gold.mjs
@@ -1,3 +1,13 @@
+// import { getUserProfile } from './profile.mjs';
+
+// window.addEventListener('load', async () => {
+//   const userProfile = await getUserProfile();
+
+//   // Update the gold count
+//   const goldCountElement = document.getElementById('gold-count');
+//   goldCountElement.textContent = `${userProfile.credits} Gold`;
+// });
+
 import { getUserProfile } from './profile.mjs';
 
 window.addEventListener('load', async () => {
@@ -5,6 +15,13 @@ window.addEventListener('load', async () => {
 
   // Update the gold count
   const goldCountElement = document.getElementById('gold-count');
-  goldCountElement.textContent = `${userProfile.credits} Gold`;
-});
+  if (goldCountElement) {
+    goldCountElement.textContent = `${userProfile.credits} Gold`;
+  }
 
+  // Update the gold count in profile
+  const goldCountProfileElement = document.getElementById('gold-count-profile');
+  if (goldCountProfileElement) {
+    goldCountProfileElement.textContent = `${userProfile.credits} Gold`;
+  }
+});
