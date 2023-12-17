6582133c6feaff5cf67ac186d7136c89342f022c
Eirik Kirkeli <knirkeli88@msn.com>
2023-12-17 21:58:34 +0100

    Update navbar height and alignment

--- header/navitems.mjs
+++ header/navitems.mjs
@@ -1,11 +1,39 @@
+// import { checkAccessToken } from '../auth/auth.mjs';
+
+// const nav = document.querySelector('.navbar'); // Select the navbar
+// const loginNav = document.querySelector('#log-in-nav');
+// const signUpNav = document.querySelector('#sign-up-nav');
+
+// if (checkAccessToken()) {
+//     // If access token exists, remove 'Log In' and 'Sign Up' and add 'Profile' and 'Log Out'
+//     loginNav.remove();
+//     signUpNav.remove();
+
+//     const profileNav = document.createElement('a');
+//     profileNav.className = 'navbar-brand';
+//     profileNav.href = 'profile.html';
+//     profileNav.textContent = 'Profile';
+
+//     const logoutNav = document.createElement('a');
+//     logoutNav.className = 'navbar-brand';
+//     logoutNav.href = '#';
+//     logoutNav.textContent = 'Log Out';
+//     logoutNav.id = 'logout';
+
+//     nav.appendChild(profileNav);
+//     nav.appendChild(logoutNav);
+// } else {
+//     // If access token does not exist, do nothing or add 'Log In' and 'Sign Up' if they are not already there
+// }
+
 import { checkAccessToken } from '../auth/auth.mjs';
 
-const nav = document.querySelector('.navbar'); // Select the navbar
+const navItemsContainer = document.querySelector('.navbar .ml-auto');
 const loginNav = document.querySelector('#log-in-nav');
 const signUpNav = document.querySelector('#sign-up-nav');
 
 if (checkAccessToken()) {
-    // If access token exists, remove 'Log In' and 'Sign Up' and add 'Profile', 'Bids', and 'Log Out'
+    // If access token exists, remove 'Log In' and 'Sign Up' and add 'Profile' and 'Log Out'
     loginNav.remove();
     signUpNav.remove();
 
@@ -14,20 +42,14 @@ if (checkAccessToken()) {
     profileNav.href = 'profile.html';
     profileNav.textContent = 'Profile';
 
-    const bidsNav = document.createElement('a');
-    bidsNav.className = 'navbar-brand';
-    bidsNav.href = 'bids.html';
-    bidsNav.textContent = 'Bids';
-
     const logoutNav = document.createElement('a');
     logoutNav.className = 'navbar-brand';
     logoutNav.href = '#';
     logoutNav.textContent = 'Log Out';
     logoutNav.id = 'logout';
 
-    nav.appendChild(profileNav);
-    nav.appendChild(bidsNav);
-    nav.appendChild(logoutNav);
+    navItemsContainer.appendChild(profileNav);
+    navItemsContainer.appendChild(logoutNav);
 } else {
     // If access token does not exist, do nothing or add 'Log In' and 'Sign Up' if they are not already there
 }
